9e05edef795f45f6ddf9b183d94d66ae74842e60
Sara Walker <sarawall1187@gmail.com>
2019-03-12 18:17:35 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,8 +14,8 @@ module FormsLab
     post '/pirates' do
       @pirate = Pirate.new(params[:pirate])
       params[:pirate][:ships].each do |details|
-
-      @ships = Ship.new(details)
+        Ship.new(details)
+      end
 
       erb :'pirates/show'
     end
